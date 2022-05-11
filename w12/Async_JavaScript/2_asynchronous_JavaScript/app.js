7d36b300c81ccd595b81d284f6d5780b10ce04f2
Sky00l <207410290@gms.tku.edu.tw>
2022-05-11 19:19:52 +0800

    W12-P1: Making soup demo for Async JavaScript

---
+++ w12/Async_JavaScript/2_asynchronous_JavaScript/app.js
@@ -0,0 +1,27 @@
+// Make Soup
+// boil water 10 min
+// chop carrots
+// add carrots boil for 5 min
+// chop onion
+// add onion boil for 5 min
+// BROWSER!!!!! Fetch Data, Get Geolocation, setTimeout, setTimer etc
+
+boilingWater();
+console.log(`chop carrots`);
+
+function boilingWater() {
+  console.log('boiling...');
+    // 非同步function
+    setTimeout(() => {
+      console.log('boiling done.');
+      console.log('add carrots');
+      setTimeout(()=> {
+        console.log('carrots boiling done')
+        console.log('add onions');
+        setTimeout(()=> {
+          console.log('omnion boiling done')
+        }, 500)
+      }, 2000);
+    }, 3000);
+  }
+
